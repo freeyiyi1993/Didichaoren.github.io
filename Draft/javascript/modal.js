8b4e67c7bf0f6191e294421871983636267f118b
Didichaoren <didichaoren@qq.com>
2014-10-27 10:34:21 +0800

    Update

--- Draft/javascript/modal.js
+++ Draft/javascript/modal.js
@@ -9,11 +9,13 @@ $(document).ready(function(){//规定弹窗动作但是没有进行绑定
             var $this   = $(this);
             var modalName = $this.attr('data-modal');//获取用户的输入标记
             $(modalName).show();//识别了用户定义的id之后就直接显示
-            $(modalName).find('.content').animate({height:'show'});//不知道为什么仅仅执行了一次！
+            $(modalName).find('.content').animate({height:'show'});
         });
 
     //改变id为close的按钮点击隐藏
     $('.close').click(function(){//直接关闭
         $('.modal').hide();
+        $('.content').hide({height:'hide'},"slow");
     })
 });
+
