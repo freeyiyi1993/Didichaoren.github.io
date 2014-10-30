6a0f7680d2b12c212025dc7f9b22d561b6a7c3e9
Didichaoren <didichaoren@qq.com>
2014-10-30 12:42:59 +0800

    Update

--- Draft/javascript/navs.js
+++ Draft/javascript/navs.js
@@ -4,6 +4,6 @@
 $(document).ready(function(){//这样不能每个都改 愁人
 
     var height = $('nav').find('.form').height();
-    alert(height);
+//    alert(height);
     $('nav').find('.form ').css({"margin-top":((50 - height)/2)+"px","margin-bottom":((50 - height)/2)+"px"});
 });
