e57a5d339ad1b2e58ca5d8a4f52b33bcd81c7e9c
Didichaoren <didichaoren@qq.com>
2014-10-31 14:45:19 +0800

    Update

--- Draft/javascript/navs.js
+++ Draft/javascript/navs.js
@@ -4,18 +4,18 @@
 $(document).ready(function(){
 
     var height5 = $('nav').find('.form input.input-xl').parents(".form").height();
-    $('nav').find('.form input.input-xl').parents(".form").css({"margin-top":((50 - height5)/2)+"px","margin-bottom":((50 - height5)/2)+"px"});
+    $('nav').find('.form input.input-xl').parents(".form").css({"margin-top":((50 - height5)/2)+"px"});
 
     var height4 = $('nav').find('.form input.input-lg').parents(".form").height();
-    $('nav').find('.form input.input-lg').parents(".form").css({"margin-top":((50 - height4)/2)+"px","margin-bottom":((50 - height4)/2)+"px"});
+    $('nav').find('.form input.input-lg').parents(".form").css({"margin-top":((50 - height4)/2)+"px"});
 
     var height = $('nav').find('.form').height();
-    $('nav').find('.form ').css({"margin-top":((50 - height)/2)+"px","margin-bottom":((50 - height)/2)+"px"});
+    $('nav').find('.form ').css({"margin-top":((50 - height)/2)+"px"});
 
     var height2 = $('nav').find('.form input.input-sm').parents(".form").height();
-    $('nav').find('.form input.input-sm').parents(".form").css({"margin-top":((50 - height2)/2)+"px","margin-bottom":((50 - height2)/2)+"px"});
+    $('nav').find('.form input.input-sm').parents(".form").css({"margin-top":((50 - height2)/2)+"px"});
 
     var height3 = $('nav').find('.form input.input-xs').parents(".form").height();
-    $('nav').find('.form input.input-xs').parents(".form").css({"margin-top":((50 - height3)/2)+"px","margin-bottom":((50 - height3)/2)+"px"});
+    $('nav').find('.form input.input-xs').parents(".form").css({"margin-top":((50 - height3)/2)+"px"});
 
 });
