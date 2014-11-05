3ae30a651515fcacc2c264fda956c39acdb07461
Didichaoren <didichaoren@qq.com>
2014-11-05 14:07:05 +0800

    Update

--- Draft/javascript/dropdowns.js
+++ Draft/javascript/dropdowns.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     $('.btn-dropdown').click(function(){
         var $this   = $(this);
         var dropdownName = $this.attr('data-dropdown');//获取用户的输入标记(ul)
-        $(dropdownName).toggle();//识别了用户定义的id之后就直接显示
+        $(dropdownName).toggle(200);//识别了用户定义的id之后就直接显示
     });
 
     //点击其他地方隐藏点击隐藏(暂时不会)
