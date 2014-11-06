01f5d222922a347d3d781ead215ecec0c3c40e30
Didichaoren <didichaoren@qq.com>
2014-11-06 17:47:55 +0800

    Update

--- Draft/javascript/dropdowns.js
+++ Draft/javascript/dropdowns.js
@@ -9,15 +9,17 @@ $(document).ready(function(){
         $(dropdownName).toggle(200);//识别了用户定义的id之后就直接显示
     });
 
-    //点击其他地方隐藏点击隐藏
-//    $(document).bind("click",function(e){
-//        var target  = $(e.target);//表示当前对象，切记，如果没有e这个参数，即表示整个BODY对象
-//        if(target.closest(dropdownName).length == 0){
-//            $(dropdownName).hide();
-//        }
-//    });
+//    //点击其他地方隐藏点击隐藏
+//    if($('.dropdown').find('.dropdownlist').css({'disaplay':'block'})) {
+//        $(document).bind("click", function (e) {
+//            var target = $(e.target);//表示当前对象，切记，如果没有e这个参数，即表示整个BODY对象
+//            if (target.closest(dropdownName).length == 0) {
+//                $(dropdownName).hide();
+//            }
+//        });
+//    }
 
-    //解决divider的大小问题,效率很可能是问题。
+    //解决divider的大小自适应,效率很可能是问题。
     var sum = $('.dropdown').find('.divider').parent('.dropdown-list');
     for (i = 0; i < sum.length; i++) {
         var width = $('.dropdown').find('.divider').eq(i).parent('.dropdown-list').width();
@@ -36,7 +38,6 @@ $(document).ready(function(){
                 "border-left": (width / 2) + "px solid transparent",
                 "border-right": (width / 2) + "px solid transparent"
             });
-
         }
     });
 
