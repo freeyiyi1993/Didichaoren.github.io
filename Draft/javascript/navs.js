08c4cc3297a85bae6af97323cd6cb0edc68303ed
Didichaoren <didichaoren@qq.com>
2014-11-19 16:00:34 +0800

    Update

--- Draft/javascript/navs.js
+++ Draft/javascript/navs.js
@@ -2,20 +2,24 @@
 
 
 $(document).ready(function(){
+    /*解决使用forms时的垂直居中问题*/
+    $('.nav').each(function(){
 
-    var height5 = $('nav').find('.form input.input-xl').parents(".form").height();
-    $('nav').find('.form input.input-xl').parents(".form").css({"margin-top":((50 - height5)/2)+"px"});
+        var height5 = $(this).find('.form input.input-xl').parents(".form").height();
+        $(this).find('.form input.input-xl').parents(".form").css({"margin-top":((50 - height5)/2)+"px"});
 
-    var height4 = $('nav').find('.form input.input-lg').parents(".form").height();
-    $('nav').find('.form input.input-lg').parents(".form").css({"margin-top":((50 - height4)/2)+"px"});
+        var height4 = $(this).find('.form input.input-lg').parents(".form").height();
+        $(this).find('.form input.input-lg').parents(".form").css({"margin-top":((50 - height4)/2)+"px"});
 
-    var height = $('nav').find('.form').height();
-    $('nav').find('.form ').css({"margin-top":((50 - height)/2)+"px"});
+        var height = $(this).find('.form').height();
+        $(this).find('.form ').css({"margin-top":((50 - height)/2)+"px"});
 
-    var height2 = $('nav').find('.form input.input-sm').parents(".form").height();
-    $('nav').find('.form input.input-sm').parents(".form").css({"margin-top":((50 - height2)/2)+"px"});
+        var height2 = $(this).find('.form input.input-sm').parents(".form").height();
+        $(this).find('.form input.input-sm').parents(".form").css({"margin-top":((50 - height2)/2)+"px"});
 
-    var height3 = $('nav').find('.form input.input-xs').parents(".form").height();
-    $('nav').find('.form input.input-xs').parents(".form").css({"margin-top":((50 - height3)/2)+"px"});
+        var height3 = $(this).find('.form input.input-xs').parents(".form").height();
+        $(this).find('.form input.input-xs').parents(".form").css({"margin-top":((50 - height3)/2)+"px"});
+
+    });
 
 });
