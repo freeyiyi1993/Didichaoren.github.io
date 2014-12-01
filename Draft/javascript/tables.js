782b5c1c21bb9879e65823484b3bf9c933dc1746
Didichaoren <didichaoren@qq.com>
2014-12-01 15:11:15 +0800

    update

--- Draft/javascript/tables.js
+++ Draft/javascript/tables.js
@@ -1,25 +1,38 @@
-$(document).ready(function(){
-    var thclone = $('.table-headfixed table').clone();
-    var obj = $('<div class="thclone table-responsive"></div>');
-    obj.insertBefore('.table-headfixed');
-    thclone.prependTo(obj);
-    var td_num = 0;
-    $('.thclone').find('thead').find('th').each(function () {
-        var td_width = $('.table-headfixed').find('tbody').find('td:nth-child('+(td_num+1)+')').width();
-        console.log(td_width);
-        $(this).width(td_width);
-        $('.table-headfixed').find('tbody').find('td:nth-child('+(td_num+1)+')').width(td_width)
-        td_num++;
-    });
-    $('.thclone tbody').hide();
-    $('.table-headfixed thead').hide();
-    $(window).resize(function () {
-        var td_num = 0;
-        $('.thclone').find('thead').find('th').each(function () {
-            var td_width = $('.table-headfixed').find('tbody').find('td:nth-child('+(td_num+1)+')').width();
-            console.log(td_width);
-            $(this).width(td_width);
-            td_num++;
-        });
-    });
-});
+(function ($) {
+    var methods = {
+        init: function (options) {
+            var $this = $(this);
+            var td_num = 0;
+            var thclone = $this.find('table').clone();
+            var obj = $('<div class="thclone table-responsive"></div>');
+            $this.before(obj);
+            obj.prepend(thclone);
+            $this.prev('.thclone').find('th').each(function () {
+                var td_width = $this.find('tbody').find('td:nth-child('+(td_num+1)+')').width();
+                $(this).width(td_width);
+                $this.find('tbody').find('td:nth-child('+(td_num+1)+')').width(td_width)
+                td_num++;
+            });
+            $this.prev('.thclone').find('tbody').hide();
+            $this.find('thead').hide();
+            $(window).resize(function () {
+                td_num = 0;
+                $this.prev('.thclone').find('th').each(function () {
+                    var td_width = $this.find('tbody').find('td:nth-child('+(td_num+1)+')').width();
+                    $(this).width(td_width);
+                    td_num++;
+                });
+            });
+        }
+    };
+    $.fn.headFixed = function (options) {
+        var method = arguments[0];
+        if ( methods[method] ) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist' );
+        }
+    };
+})(jQuery);
