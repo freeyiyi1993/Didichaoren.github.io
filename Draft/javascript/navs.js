9d031942c1f15450c48c12b127c4c2b6fd8f2430
Didichaoren <didichaoren@qq.com>
2014-12-03 15:05:48 +0800

    Update

--- Draft/javascript/navs.js
+++ Draft/javascript/navs.js
@@ -6,19 +6,39 @@ $(document).ready(function(){
     $('.nav').each(function(){
 
         var height5 = $(this).find('.form input.input-xl').parents(".form").height();
-        $(this).find('.form input.input-xl').parents(".form").css({"margin-top":((50 - height5)/2)+"px"});
+        $(this).find('.form input.input-xl').parents(".form")
+            .css({
+                "margin-top":((50 - height5)/2)+"px",
+                "margin-bottom":((50 - height5)/2)+"px"
+            });
 
         var height4 = $(this).find('.form input.input-lg').parents(".form").height();
-        $(this).find('.form input.input-lg').parents(".form").css({"margin-top":((50 - height4)/2)+"px"});
+        $(this).find('.form input.input-lg').parents(".form")
+            .css({
+                "margin-top":((50 - height4)/2)+"px",
+                "margin-bottom":((50 - height4)/2)+"px"
+            });
 
         var height = $(this).find('.form').height();
-        $(this).find('.form ').css({"margin-top":((50 - height)/2)+"px"});
+        $(this).find('.form ')
+            .css({
+                "margin-top":((50 - height)/2)+"px",
+                "margin-bottom":((50 - height)/2)+"px"
+            });
 
         var height2 = $(this).find('.form input.input-sm').parents(".form").height();
-        $(this).find('.form input.input-sm').parents(".form").css({"margin-top":((50 - height2)/2)+"px"});
+        $(this).find('.form input.input-sm').parents(".form")
+            .css({
+                "margin-top":((50 - height2)/2)+"px",
+                "margin-bottom":((50 - height2)/2)+"px"
+            });
 
         var height3 = $(this).find('.form input.input-xs').parents(".form").height();
-        $(this).find('.form input.input-xs').parents(".form").css({"margin-top":((50 - height3)/2)+"px"});
+        $(this).find('.form input.input-xs').parents(".form")
+            .css({
+                "margin-top":((50 - height3)/2)+"px",
+                "margin-bottom":((50 - height3)/2)+"px"
+            });
 
     });
 
