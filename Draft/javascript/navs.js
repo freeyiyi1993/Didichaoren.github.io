ff9e54402c8d98f40f346497f44be87495e00c78
Didichaoren <didichaoren@qq.com>
2014-12-04 17:12:47 +0800

    Update

--- Draft/javascript/navs.js
+++ Draft/javascript/navs.js
@@ -2,47 +2,11 @@
 
 
 $(document).ready(function(){
-    /*解决使用forms时的垂直居中问题*/
+    /*暂时解决解决使用forms时的垂直居中问题，编译css之后删除*/
     $('.nav').each(function(){
-        var $this = $(this);
-        var Height1 =  $this.height();
-        var Height = Height1 - 0.2 ;
-
-        var height5 = $this.find('.form input.input-xl').parents(".form").height();
-        $this.find('.form input.input-xl').parents(".form")
-            .css({
-                "margin-top":((Height - height5)/2)+"px",
-                "margin-bottom":((Height - height5)/2)+"px"
-            });
-
-        var height4 = $this.find('.form input.input-lg').parents(".form").height();
-        $this.find('.form input.input-lg').parents(".form")
-            .css({
-                "margin-top":((Height - height4)/2)+"px",
-                "margin-bottom":((Height - height4)/2)+"px"
-            });
-
-        var height = $this.find('.form').height();
-        $this.find('.form ')
-            .css({
-                "margin-top":((Height - height)/2)+"px",
-                "margin-bottom":((Height - height)/2)+"px"
-            });
-
-        var height2 = $this.find('.form input.input-sm').parents(".form").height();
-        $this.find('.form input.input-sm').parents(".form")
-            .css({
-                "margin-top":((Height - height2)/2)+"px",
-                "margin-bottom":((Height - height2)/2)+"px"
-            });
-
-        var height3 = $this.find('.form input.input-xs').parents(".form").height();
-        $this.find('.form input.input-xs').parents(".form")
-            .css({
-                "margin-top":((Height - height3)/2)+"px",
-                "margin-bottom":((Height - height3)/2)+"px"
-            });
-
+        $(this).find('.form').find('.input-group').css({
+            'margin':'calc(0.5em - 1px) 0'
+        });
     });
 
 });
