ab85b27ba555a88905270c377eca45ce6dde5396
Didichaoren <didichaoren@qq.com>
2014-12-05 14:49:59 +0800

    Update

--- Draft/javascript/navs.js
+++ Draft/javascript/navs.js
@@ -1,12 +1,13 @@
 
 
-
 $(document).ready(function(){
     /*暂时解决解决使用forms时的垂直居中问题，编译css之后删除*/
     $('.nav').each(function(){
-        $(this).find('.form').find('.input-group').css({
-            'margin':'calc(0.5em - 1px) 0'
-        });
+        var $this = $(this);
+        $this.find('.form ').css({'margin-top':'calc(0.5em - 1px)'});
+        $this.find('.form .input-xl').parents(".form").css({'margin-top':'calc(0.2em - 1px)'});
+        $this.find('.form .input-lg').parents(".form").css({'margin-top':'calc(0.3636em - 1px)'});
+        $this.find('.form .input-sm').parents(".form").css({'margin-top':'calc(0.65em - 1px)'});
+        $this.find('.form .input-xs').parents(".form").css({'margin-top':'calc(0.8em - 1px)'});
     });
-
 });
